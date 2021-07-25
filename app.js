6bfce77172b0b6a5cdd36eb1b9a206e0f5fac983
Pyristix <61607183+Pyristix@users.noreply.github.com>
2021-07-25 16:12:43 -0500

    Sign in/Sign up page & Functional Downloading
    Created the Pug and CSS for the sign in/sign up pages, cleaned up some of the previous Pug files, created most of the necessary routes, and created a functional download for the Screen Translator Installer.

--- app.js
+++ app.js
@@ -9,25 +9,26 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+//View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//Middleware setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Setting indexRouter to base path
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+//Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+//Error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('homepage', { title: 'Express', username: 'TestUsername'});
+  res.render('homepage'/*, {username: 'TestUsername'}*/);
+});
+
+router.get('/download', function(req, res, next) {
+  res.download("./dist/screen-translator-1.0.0.exe");
+});
+
+router.get('/logout', function(req, res, next) {
+  res.render('logout');
+});
+
+router.get('/sign_in', function(req, res, next) {
+  res.render('sign_in', {signing_up: false, username: "test"});
+});
+
+router.get('/sign_up', function(req, res, next) {
+  res.render('sign_in', {signing_up: true});
 });
 
 module.exports = router;
